81b8454a1d47e9cbf26dbded1584c9cfbe78407f
yong-jie <viorama@gmail.com>
2019-01-19 16:07:31 +0800

    Add physics engine

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  // Specifies the name of the functions that should be called.
   scene: {
     preload: preload,
     create: create,
@@ -16,14 +24,15 @@ function preload() {
   this.load.image("ground", "assets/platform.png");
   this.load.image("star", "assets/star.png");
   this.load.image("bomb", "assets/bomb.png");
-  this.load.spritesheet("dude", "assets/dude.png", {
-    frameWidth: 32,
-    frameHeight: 48,
+  this.load.spritesheet("puyos", "assets/bubble_spritesheet.png", {
+    frameWidth: 30,
+    frameHeight: 30,
   });
 }
 
 function create() {
-  this.add.image(400, 300, "sky");
+  //this.add.image(400, 300, "sky");
+  this.add.sprite(400, 300, "puyos", 3);
 }
 
 function update() {}
