f56fcebe0fc27bfe67310b0c079f83e0fa9f595a
iNekox3 <inekox3@hotmail.com>
2019-01-19 21:15:48 +0800

    Spawn randomly colored puyos

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const puyo_sprite_width = 30; // Square so same height.
 const default_puyo_spawn_column = 2; // Third column.
 const puyo_fall_velocity = 80;
 const puyo_fall_high_velocity = 500;
+const number_of_puyo_animation_frames = 3;
 const game_over_grid_coord = [75, 15];
 
 var config = {
@@ -34,10 +35,19 @@ let last_left_right_pressed = 0; // To prevent overly quick repeats.
 const game_state_matrix = new Array(6).fill(new Array(12).fill(null)); // Column then row.
 const game_height_map = new Array(6).fill(11); // Largest index with no puyos.
 
+const generateRandomColor = () => {
+  let index = Math.floor(Math.random() * 5);
+  return index * number_of_puyo_animation_frames;
+};
+
 const spawn_new_puyo = scene => {
   falling_puyo_column = default_puyo_spawn_column;
+
+  const puyo_color = generateRandomColor();
+
   const new_puyo_x = compute_falling_puyo_x();
-  falling_puyo = scene.physics.add.sprite(new_puyo_x, 0, "puyos", 0);
+  falling_puyo = scene.physics.add.sprite(new_puyo_x, 0, "puyos", puyo_color);
+  falling_puyo.color = puyo_color;
   falling_puyo.setOrigin(0, 0);
   falling_puyo.setVelocityY(puyo_fall_velocity);
 };
