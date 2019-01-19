84d5b9eac5bd17d6c964683437087ac29e835779
yong-jie <viorama@gmail.com>
2019-01-19 17:39:51 +0800

    Remove magic numbers

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const number_of_columns = 6;
+const number_of_rows = 12;
+const puyo_sprite_width = 30; // Square so same height.
+
 var config = {
   type: Phaser.AUTO,
-  width: 180,
-  height: 360,
+  width: puyo_sprite_width * number_of_columns,
+  height: puyo_sprite_width * number_of_rows,
   physics: {
     default: "arcade",
     arcade: {
@@ -16,18 +20,18 @@ var config = {
   },
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
   this.load.spritesheet("puyos", "assets/bubble_spritesheet.png", {
-    frameWidth: 30,
-    frameHeight: 30,
+    frameWidth: puyo_sprite_width,
+    frameHeight: puyo_sprite_width,
   });
 }
 
 function create() {
-  //this.add.image(400, 300, "sky");
-  this.add.sprite(400, 300, "puyos", 3);
+  let puyo = this.physics.add.sprite(90, 175, "puyos", 0);
+  puyo.setVelocityY(80);
 }
 
 function update() {}
